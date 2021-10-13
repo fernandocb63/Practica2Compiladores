3d55812f707a7965057fe7c42ea628b5e4b7f2da
FernandoC <fernandocb63@gmail.com>
2021-10-13 18:17:16 -0500

    Final Commit

--- src/routes/noticia.js
+++ src/routes/noticia.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
 const axios = require ('axios');
 const apiKey = '5f869e794825494b846a6738bee04db2';
-const getnews = require('../controllers/noticia.controllers');
+const {getnews,newsearch} = require('../controllers/noticia.controllers');
 
 
 
-router.get('/', getnews);
+router.get('/', (req,res)=>{
+    let seq;
+    if(!req.query.s){
+        seq=getnews();
+    }
+    else{
+        seq=newsearch(req.query.s);
+    }
+    seq.then(response=>{
+        let notis=response.data.articles;
+        res.render('main',{layout:'index',noti:notis})
+    })
+});
 
 
 
