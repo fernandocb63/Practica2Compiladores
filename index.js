00bdce79b1f07095008a44c2efb13ee4d8c2e05c
FernandoC <fernandocb63@gmail.com>
2021-10-13 13:03:01 -0500

    Second commit

--- index.js
+++ index.js
@@ -21,20 +21,10 @@ router.get('/', (req, res) => {
 if (process.env.NODE_ENV === 'dev') {
     require('dotenv').config();
 }
+ 
 
-// app.get('/noticias', (req,res,next)=>{
-//     const search=req.query.articles;
-//     noticias.find({title:{$regex: search, $options: '$i'}})
-//     .then(data=>{
-//         res.send(data);
-//     })
-// })
-// router.get('/noticias', function(req, res, next) {
-//     var title = req.params.title;
-//     noticiaControllers.find({title: title}, function (err, products) {
-//         res.send(title);
-//     });
-// });
+
+
 app.listen(port, () => {
     console.log('App is listening in port: ' + port);
 })
